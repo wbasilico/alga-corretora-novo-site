ac0b664544eb163c8a306fb3c5c559c5c7611bbb
wbasilico <wilbert.basilico@gmail.com>
2026-02-27 17:35:23 -0300

    Ajuste do scroll_4

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
-import Footer from './components/Footer'; // Importe o Footer aqui
+import Footer from './components/Footer';
 import PoliticaPrivacidade from './pages/PoliticaPrivacidade';
 import TermosDeUso from './pages/TermosDeUso';
 
 function App() {
   return (
     <Router>
-      <div id="top"></div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
-        <Route path="/termos-uso" element={<TermosDeUso />} />
-      </Routes>
-      <Footer /> {/* Coloque o Footer aqui, fora do Routes e abaixo dele */}
+      {/* Colocamos o ID numa div que envolve os componentes principais */}
+      <div id="top" className="app-container">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/politica-privacidade" element={<PoliticaPrivacidade />} />
+            <Route path="/termos-uso" element={<TermosDeUso />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
